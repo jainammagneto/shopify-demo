c926eb6139348f106fbbe1e8c7d611f32f1d995c
Jainam <jainamm.magneto@gmail.com>
2022-03-04 10:46:23 +0530

    Chagnes

--- assets/application.js
+++ assets/application.js
@@ -6,24 +6,40 @@ $('.toggle-menu').click(function () {
     $('#menu').toggleClass('open');
 });
 
-$(function () {
-
-    $(document).on('click', 'a.page-scroll', function (event) {
-        var $anchor = $(this);
-        $('html,body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 45)
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
 
 
 
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ {
-        $('header').addClass('opaque');
-    } else {
-        $('header').removeClass('opaque');
+// Video section JS
+
+var videoPlayButton,
+    videoWrapper = document.getElementsByClassName('video-wrapper')[0],
+    video = document.getElementsByTagName('video')[0],
+    videoMethods = {
+        renderVideoPlayButton: function () {
+            if (videoWrapper.contains(video)) {
+                this.formatVideoPlayButton()
+                video.classList.add('has-media-controls-hidden')
+                videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
+                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
+            }
+        },
+
+        formatVideoPlayButton: function () {
+            videoWrapper.insertAdjacentHTML('beforeend', '\
+                <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
+                    <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
+                    <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
+                </svg>\
+            ')
+        },
+
+        hideVideoPlayButton: function () {
+            video.play()
+            videoPlayButton.classList.add('is-hidden')
+            video.classList.remove('has-media-controls-hidden')
+            video.setAttribute('controls', 'controls')
+        }
     }
-});
+
+videoMethods.renderVideoPlayButton()
